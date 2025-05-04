2732ac0302607d1125a53649b380fa8ee7f78254
Miłosz Bochenek <miloszbochenek13@gmail.com>
2025-05-04 20:48:13 +0200

    Adding Post model with controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,13 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [ :new, :create ]
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "posts/index"
+  get "posts/new"
+  get "posts/create"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -12,4 +15,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "posts#index"
+  devise_for :users
+  resources :posts, only: [ :index, :new, :create ]
 end
